ea1ea1a6a52bcc8db8afe35ff818e5916791f24c
vadivukkarasijp <40274288+vadivukkarasijp@users.noreply.github.com>
2018-07-23 15:59:51 +0530

    Update dot.java

--- dot.java
+++ dot.java
@@ -4,8 +4,6 @@ public class dot{
 	public static void main(String[]args){
 		Scanner sc=new Scanner(System.in);
 		String a=sc.next();
-		StringBuffer sb=new StringBuffer(a);
-		sb.append(".");
-		System.out.println(sb);
+		System.out.println(a+".");
 	}			
 	}
